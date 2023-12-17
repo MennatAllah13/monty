71fcb67a8596786136be0a17834637dfa738f316
Mennat Allah <mennatallahhisham99@gmail.com>
2023-12-17 03:14:26 -0800

    Add print.c

--- print.c
+++ print.c
@@ -95,3 +95,25 @@ void print_str_func(stack_t **head, unsigned int counter)
 	printf("\n");
 }
 
+/**
+ * pop_func - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void pop_func(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		stack_free(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
